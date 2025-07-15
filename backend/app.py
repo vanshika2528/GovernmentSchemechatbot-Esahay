7aa25f11bb4aa49fcd3cc933cfd79f4ebbb9182c
Princee Patel <22it106@charusat.edu.in>
2025-07-15 19:30:44 +0530

    Added backend files

--- backend/app.py
+++ backend/app.py
@@ -26,36 +26,17 @@ TWEAKS = {
     }
 
 
-
-# LANGFLOW_URL = 'http://127.0.0.1:7860/flow/05972445-0fd5-401c-b4c2-de2fdc3cebaa'
-# API_KEY = 'sk-0toha2qOiH8OX9KI8sqszx2sYhFTlH2fSZtUcpgL9Jc'  # Replace with your actual API key
-
-
 @app.route('/api/chat', methods=['POST'])
 def chat():
         data = request.get_json()
         message = data.get('input_value')
         
-        # Process the message using run_flow_from_json
-        # flow_input = {
-        #     "input_value": message,
-        #     "tweaks": TWEAKS
-        # }
-        # headers = {
-        #     'Authorization': f'Bearer {API_KEY}',
-        #     'Content-Type': 'application/json'
-        # }
+    
         print(f"User query: {message}")
-        # # Forward the request to the Langflow API
-        # response = requests.get(LANGFLOW_URL, json=flow_input, headers=headers)
+      
         try:
           response = run_flow_from_json(flow="Esahay Chatbot.json", input_value=message, tweaks=TWEAKS)
-    # Print the response
-        #   print("Response object:", response)
-    # if hasattr(response, 'output_value'):
-    #     print(response.output_value)
-    # else:
-    #     print('No response')
+   
           if response and len(response) > 0:
             run_output = response[0]
             if hasattr(run_output, 'outputs') and run_output.outputs:
@@ -73,21 +54,12 @@ def chat():
             print(f"Error: {str(e)}")
 
         
-        
-        # if response.status_code == 200:
-        #     flow_output = response.json()
-        #     # Extract the output from the flow_output
-        #     output = flow_output.get('output_value', 'No response')
-        # else:
-        #     output = 'Error: Unable to process the request'
-        
+
         return jsonify({'output': message})
 
 @app.route('/')
 def home():
         return jsonify(message="Welcome to the backend API!")
 
-# if __name__ == '__main__':
-        # app.run(debug=True)
 if __name__ == '__main__':
         app.run(debug=True, host="0.0.0.0", port=5000)  
